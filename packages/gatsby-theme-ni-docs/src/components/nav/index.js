70211c6742faa3cab96fedbf7c49374db1926cdb
Fred Visser <fred.visser@ni.com>
2019-06-28 15:54:14 -0500

    temp URL change

--- packages/gatsby-theme-ni-docs/src/components/nav/index.js
+++ packages/gatsby-theme-ni-docs/src/components/nav/index.js
@@ -25,7 +25,7 @@ const navConfig = {
 
 function formatValue(value) {
   return value.startsWith('/')
-    ? `https://ni.github.io/design-system${value}`
+    ? `https://fredvisser.github.io/${value}`
     : value;
 }
 
